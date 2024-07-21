9264d2fdaa0e2f46c40cc825450f0eac1be12cf8
sumuli02 <76789665+sumuli02@users.noreply.github.com>
2024-07-21 17:42:21 +0100

    Update main.py

--- one_pick/main.py
+++ one_pick/main.py
@@ -17,15 +17,20 @@ import data_prep
 sys.path.append(os.path.abspath('../one_pick'))
 import PSO
 
+num = 2
+con = 4
 # Get data
-_, data1 = data_prep.get_MNIST(10, 2)
-_, data2 = data_prep.get_MNIST(10, 4)
+_, data1 = data_prep.get_MNIST(100, num)
+_, data2 = data_prep.get_MNIST(100, con)
 
 ### Run PSO
 pso = PSO.PSO()
 # k= parent size, swarm_size=number of parameter set in one iteration
-result = pso.run([2, 4], [data1, data2], swarm_size=5,options={'c1': 1.5, 'c2':1.5, 'w':0.5, 'k':2, 'p':1}, iters=5)
-pso.test([2, 4], [data1, data2], result)
+result = pso.run([num, con], [data1, data2], swarm_size=5,options={'c1': 1.5, 'c2':1.5, 'w':0.5, 'k':2, 'p':1}, iters=5)
+
+_, data1 = data_prep.get_MNIST(200, num)
+_, data2 = data_prep.get_MNIST(200, con)
+pso.test([num, con], [data1, data2], result)
 
 ###
 
